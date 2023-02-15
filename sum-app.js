0bd28983e1b83f82f4e82090c28534659212dc5f
kiraGit67 <kirsten@sais.de>
2023-02-15 14:51:58 +0100

    fix: added solving code to sum-app.js

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,42 @@
 class SumApp {
   // Add you solution here
+  constructor() {
+    this.numbers = [];
+  }
+
+  //methods
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  getSum() {
+    let sum = 0;
+
+    if (this.numbers.length === 0) {
+      sum = 0;
+    } else {
+      for (let number of this.numbers) {
+        sum += number;
+      }
+    }
+    return sum;
+  }
+
+  reset() {
+    this.numbers = [];
+  }
 }
+
+const sumApp = new SumApp();
+
+sumApp.addNumber(1);
+sumApp.addNumber(2);
+
+console.log(sumApp.numbers.length); // 2
+
+console.log(sumApp.getSum()); // 3
+
+sumApp.reset();
+
+console.log(sumApp.numbers.length); // 0
+console.log(sumApp.getSum()); // 0
